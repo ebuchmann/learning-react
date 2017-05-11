2a0beb4011e4434a574ea6bfb2270684fdfaa1b8
Eric Buchmann <me@ebuchmann.com>
2017-05-10 18:06:27 -0700

    basic routing

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const ReactRouter = require('react-router');
+const { Router, Route, Navigation } = ReactRouter;
+const createBrowserHistory = require('history/lib/createBrowserHistory');
 
 /**
  * App component
@@ -23,12 +26,14 @@ const Header = React.createClass({
   render: function() {
     return (
       <header className="top">
-        <h1>Catch
+        <h1>
+          Catch
           <span className="ofThe">
             <span className="of">of</span>
             <span className="the">the</span>
           </span>
-        Day</h1>
+          Day
+        </h1>
         <h3 className="tagline">
           <span>{this.props.tagline}</span>
         </h3>
@@ -67,7 +72,29 @@ const StorePicker = React.createClass({
   }
 });
 
+/**
+ * Not found
+ */
+
+const NotFound = React.createClass({
+  render: function() {
+    return <h1>Not Found!</h1>
+  }
+});
+
+/**
+ * Routes
+ */
+
+const routes = (
+  <Router history={createBrowserHistory()} >
+    <Route path="/" component={StorePicker} />
+    <Route path="/store/:storeId" component={App} />
+    <Route path="*" component={NotFound} />
+  </Router>
+);
+
 ReactDOM.render(
-  <App />,
+  routes,
   document.querySelector('#main'),
 );
