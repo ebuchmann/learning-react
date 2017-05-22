0f0780a22909f19e02a361e0f5e44374bb94922b
Eric Buchmann <me@ebuchmann.com>
2017-05-22 16:50:53 -0700

    basic routing

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const ReactRouter = require('react-router');
-const { Router, Route, Navigation } = ReactRouter;
+const { Router, Route, History } = ReactRouter;
 const createBrowserHistory = require('history/lib/createBrowserHistory');
 
 const h = require('./helpers');
@@ -63,9 +63,18 @@ const Inventory = React.createClass({
  */
 
 const StorePicker = React.createClass({
+  mixins: [History],
+
+  goToStore: function(e) {
+    e.preventDefault();
+
+    const storeId = this.refs.storeId.value;
+    this.history.pushState(null, `/store/${storeId}`);
+  },
+
   render: function() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
         <input type="Submit" />
